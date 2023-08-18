4b5fba36a8deb11b540121725853f4222fe022b4
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-08-18 11:33:54 +0530

    Minor code refactor

--- app/src/main/java/com/example/jetpackcomposedemo/signin/SignInRoute.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/signin/SignInRoute.kt
@@ -3,7 +3,7 @@ package com.example.jetpackcomposedemo.signin
 import androidx.compose.runtime.Composable
 
 @Composable
-fun SignInRoute(OnSignInClicked :(email: String, password: String) -> Unit){
+fun SignInRoute(OnSignInClicked :(String, String) -> Unit){
     SignInScreen(OnSignInClicked = {
         email, password ->  OnSignInClicked(email, password)
     })
