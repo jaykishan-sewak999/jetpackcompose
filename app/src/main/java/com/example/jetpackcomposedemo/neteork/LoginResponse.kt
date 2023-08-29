f3f29415e1d3b84449fcd7f1558ccdfc7a976cbd
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-08-29 10:44:07 +0530

    Added repository class

--- app/src/main/java/com/example/jetpackcomposedemo/neteork/LoginResponse.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/neteork/LoginResponse.kt
@@ -2,7 +2,7 @@ package com.example.jetpackcomposedemo.neteork
 
 import com.example.jetpackcomposedemo.signin.User
 
-data class LoginResponse(val user: User){
+data class LoginResponse(val string: String){
     var isSuccess: Boolean? =null
     var message: String?= null
 }
--- app/src/main/java/com/example/jetpackcomposedemo/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/signin/SignInViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.jetpackcomposedemo.signin
 
-class SignInViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.jetpackcomposedemo.neteork.APIService
+import com.example.jetpackcomposedemo.neteork.LoginResponse
+import com.example.jetpackcomposedemo.neteork.RetrofitClientInstance
+import kotlinx.coroutines.launch
+
+class SignInViewModel(val userRepository: UserRepository) : ViewModel() {
+
+    var loginResponse: LoginResponse by mutableStateOf(LoginResponse(""))
+    var errorMessage: String by mutableStateOf("")
+
+    fun doLogin(){
+        viewModelScope.launch {
+            loginResponse = userRepository.doLogin(User("test@test.com","123456"))
+        }
+    }
+}
+
+class SignInViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SignInViewModel::class.java)) {
+            return SignInViewModel(UserRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
