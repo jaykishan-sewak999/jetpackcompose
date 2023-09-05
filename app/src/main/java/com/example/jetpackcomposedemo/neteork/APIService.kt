79652606ee4dce140b8a95079f31d009e0dafa98
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-09-05 15:51:54 +0530

    Added required classes for list API

--- app/src/main/java/com/example/jetpackcomposedemo/neteork/APIService.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/neteork/APIService.kt
@@ -1,7 +1,9 @@
 package com.example.jetpackcomposedemo.neteork
 
 import com.example.jetpackcomposedemo.signin.User
+import com.example.jetpackcomposedemo.dashboard.UsersListResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 
@@ -12,4 +14,7 @@ interface APIService {
 
     @POST(END_URL_LOGIN_WITH_EMAIL)
     suspend fun login(@Body user: User): LoginResponse
+
+    @GET(END_URL_EMPLOYEE_LIST)
+    suspend fun fetchUsers(): UsersListResponse
 }
--- app/src/main/java/com/example/jetpackcomposedemo/signin/UserRepository.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/signin/UserRepository.kt
@@ -1,13 +1,22 @@
 package com.example.jetpackcomposedemo.signin
 
+import com.example.jetpackcomposedemo.dashboard.UsersListResponse
 import com.example.jetpackcomposedemo.neteork.LoginResponse
 import com.example.jetpackcomposedemo.neteork.RetrofitClientInstance
+import com.example.jetpackcomposedemo.neteork.RetrofitListAPIClientInstance
 
 object UserRepository {
 
     val apiService = RetrofitClientInstance.apiService
 
+    val userApiService = RetrofitListAPIClientInstance.apiService
+
     suspend fun doLogin(user: User): LoginResponse {
         return apiService.login(user)
     }
+
+    suspend fun fetchUsers(): UsersListResponse {
+        return userApiService.fetchUsers()
+    }
+
 }
